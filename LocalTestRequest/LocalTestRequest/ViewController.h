e02fda73a2b023a804f5323da35c0345ef537104
lh <feeyo@feeyodeMac-mini.local>
2017-11-15 14:05:07 +0800

    no message

---
+++ LocalTestRequest/LocalTestRequest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalTestRequest
+//
+//  Created by feeyo on 2017/11/15.
+//  Copyright © 2017年 lihuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocalTestRequest/LocalTestRequest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LocalTestRequest
+//
+//  Created by feeyo on 2017/11/15.
+//  Copyright © 2017年 lihuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
